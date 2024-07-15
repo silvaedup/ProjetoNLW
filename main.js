d547ae28842dc81a07900780a4490bf908880479
Eduardo Silva <edupsilva4330@gmail.com>
2024-07-15 00:48:28 -0300

    continuei a aula 2 adicionando a biblioteca de terceiros day.js

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-//object {}
+//biblioteca e códigos de terceiros
+const formatador = (data) => {
+  return {
+    dia: {
+      numerico: dayjs(data).format("DD"),
+      semana: {
+        curto: dayjs(data).format("ddd"),
+        longo: dayjs(data).format("dddd"),
+      },
+    },
+    mes: dayjs(data).format("MMMM"),
+    hora: dayjs(data).format("HH:mm"),
+  };
+};
+
+// object {}
 const atividade = {
   nome: "Almoço",
   data: new Date("2024-07-08 10:00"),
   finalizada: true,
 };
-
-// lista, arrey, vetor []´
-const atividades = [
+// lista, array,vector []
+let atividades = [
   atividade,
   {
     nome: "Academia em grupo",
-    data: new Date("2024-07-09 10:00"),
+    data: new Date("2024-07-09 12:00"),
     finalizada: false,
   },
   {
@@ -20,28 +34,45 @@ const atividades = [
   },
 ];
 
-//arow function
-const criaritemdeatividade = (atividade) => {
-  let input = '<input type="checkbox"';
+//atividades = [];
+
+// arrow function
+const criarintemdeatividade = (atividade) => {
+  let input = '<input type="checkbox" ';
 
   if (atividade.finalizada) {
-    input = input + "checked";
+    input += "checked";
   }
 
   input += ">";
 
-  const formatador = formatador(atividade.data);
+  const formatar = formatador(atividade.data);
 
   return `
-   <div>
+  <div> 
     ${input}
-    <span>${atividade.nome}</span>
-    <time>${atividade.data}</time>
-  </div>
+    <sapan>${atividade.nome}</span>
+    <time>
+      ${formatar.dia.semana.longo},
+      dia ${formatar.dia.numerico} 
+      de ${formatar.mes}
+      às ${formatar.hora}h </time>
+    </div>
   `;
 };
 
-const section = document.querySelector("section");
-for (let atividade of atividades) {
-  section.innerHTML += criaritemdeatividade(atividade);
-}
+const atualizarlistadeatividade = () => {
+  const section = document.querySelector("section");
+
+  //verificar se a lista está vazia
+  if (atividades.length == 0) {
+    section.innerHTML = `<p>Nenhuma atividade cadastrada.</<p>`;
+    return;
+  }
+
+  for (let atividade of atividades) {
+    section.innerHTML += criarintemdeatividade(atividade);
+  }
+};
+
+atualizarlistadeatividade();
